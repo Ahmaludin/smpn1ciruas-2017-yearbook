065e0316fe975be28d1bc7df372b4da0e550426d
Ahmaludin <ahmal54321@gmail.com>
2022-10-19 14:51:55 +0700

    Menambah file jquery, membuat navigasi, merubah header button, merubah sedikit visi & misi, fix width to max-width dan padding agar responsive.

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+// NAVIGATION AREA
+
+$(window).scroll(function () {
+  let wScroll = $(this).scrollTop();
+  const containerNav = document.querySelector('.containerNav');
+  if (wScroll > 100) {
+    containerNav.style.boxShadow = 'rgba(149, 157, 165, 0.2) 0px 8px 24px';
+  } else if (wScroll < 100) {
+    containerNav.style.boxShadow = 'none';
+  }
+});
+
 // HEADER AREA
 
 const btnHeader = document.querySelector('header a');
